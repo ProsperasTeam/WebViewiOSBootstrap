712f3697fff1c3bb8ddb84780d429ce94d348483
sayab perez <sayab@madhouse.com.mx>
2023-07-13 18:47:31 -0600

    demo
    demo

--- Marketplace Webview/ViewController.swift
+++ Marketplace Webview/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Button1: UIButton!
+    var apikey = ""
+    var sessionId = ""
+    var locale = ""
+    
+    
+    @IBOutlet weak var apikeyText: UITextField!
+    
+    @IBOutlet weak var localeText: UITextField!
+    @IBOutlet weak var sessionIdText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        overrideUserInterfaceStyle = .light
+        
+        apikeyText.text = "f0627e97-48fb-456b-ba05-c20ce8c56105"
+        localeText.text = "es-rMX"
+        sessionIdText.text = "d/fvIRnSFf2gQslGnCVm/piBOgSxZWjUxzmT8+WeiMKwvF0tFj68wpi7uTSxO8tXZCPHy2kPwC+4afQUeRVNiXhh8AIQk27SsQq1xqm/etsossdJ0tEdsWg7L6jRou9bXCDGamE0mnE="
+
         // Do any additional setup after loading the view.
     }
 
+    public override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("mi segue identifier \(segue.identifier)")
+        if (segue.identifier == "WebSegue") {
+            let view2 = segue.destination as! ViewController2
+            //_ = sender as! [Int: Any?]
+            print("el apikey que voy a enviar \(apikeyText.text)")
+            print("el sessionId que voy a enviar \(sessionIdText.text)")
+            print("el locale que voy a enviar \(localeText.text)")
+            view2.apikey = apikeyText.text ?? ""
+            view2.sessionId = sessionIdText.text ?? ""
+            view2.locale = localeText.text ?? ""
+        }
+    }
 
+    @IBAction func goWebView(_ sender: Any) {
+        self.performSegue(withIdentifier: "WebSegue", sender: nil)
+    }
 }
 
---
+++ Marketplace Webview/ViewController2.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  Marketplace Webview
+//
+//  Created by Angelo Vescio on 7/5/23.
+//
+
+import UIKit
+import WebKit
+
+class ViewController2: UIViewController {
+
+    var apikey = ""
+    var sessionId = ""
+    var locale = ""
+    var url = "https://google.com"
+    
+    @IBOutlet weak var regresarButton: UIButton!
+    @IBOutlet weak var webView: WKWebView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        overrideUserInterfaceStyle = .light
+        
+       /* print("mi info es apikey: \(apikey), locale: \(locale), sessionId: \(sessionId)")
+        url = "https://creditos-web.prosperas.com?sessionid=\(sessionId)&apikey=\(apikey)&locale=\(locale)"
+        // Do any additional setup after loading the view.
+        
+        print("url recibida ::: \(url)")
+        url = url.replacingOccurrences(of: " ", with: "")
+        print("url ajustada ::: \(url)")
+        webView.customUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Mobile/15E148 Safari/604.1"
+        
+        webView.load(NSURLRequest(url: NSURL(string: url)! as URL) as URLRequest)
+        webView.allowsBackForwardNavigationGestures = true*/
+        abrirSDK()
+        
+      
+        
+    }
+    
+    func abrirSDK(){
+        print("mi info es apikey: \(apikey), locale: \(locale), sessionId: \(sessionId)")
+        url = "https://creditos-web.prosperas.com?sessionid=\(sessionId)&apikey=\(apikey)&locale=\(locale)"
+        // Do any additional setup after loading the view.
+        
+        print("url recibida ::: \(url)")
+        url = url.replacingOccurrences(of: " ", with: "")
+        print("url ajustada ::: \(url)")
+        webView.customUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Mobile/15E148 Safari/604.1"
+        
+        webView.load(NSURLRequest(url: NSURL(string: url)! as URL) as URLRequest)
+        webView.allowsBackForwardNavigationGestures = true
+    }
+    
+
+    @IBAction func regresar(_ sender: Any) {
+        abrirSDK()
+    }
+    
+}
+
