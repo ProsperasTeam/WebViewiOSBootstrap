68b799f380d414271b468e7b3034df8c0f58843b
vesh <avescio@prosperas.com>
2023-07-16 23:50:46 -0400

    adding Podfile and others

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Marketplace Webview' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Marketplace Webview
+  pod 'Marketplace', :git => 'https://github.com/ProsperasTeam/MarketplaceSDKiOS.git', :branch => 'release', :tag => '1.0.6'
+  #LOCAL ONLY pod 'Marketplace', :path => '/Users/angelovescio/MarketplaceSDKiOS'
+
+  target 'Marketplace WebviewTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Marketplace WebviewUITests' do
+    # Pods for testing
+  end
+
+end
---
+++ ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Marketplace Webview
+//
+//  Created by Angelo Vescio on 7/5/23.
+//
+
+import UIKit
+import Marketplace
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Button1: UIButton!
+    var apikey = ""
+    var sessionId = ""
+    var locale = ""
+    
+    
+    @IBOutlet weak var apikeyText: UITextField!
+    
+    @IBOutlet weak var localeText: UITextField!
+    @IBOutlet weak var sessionIdText: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        overrideUserInterfaceStyle = .light
+        
+        apikeyText.text = "f0627e97-48fb-456b-ba05-c20ce8c56105"
+        localeText.text = "es-rMX"
+        sessionIdText.text = "d/fvIRnSFf2gQslGnCVm/pz7u8K5JTkEFY5w7KJ1rSawvF0tFj68wuL+Zuy2DrEkr8I/dqUj27iW0fg4RyMaU+jsqimIAsTVZnJjjYWpymAwyFRXa7rZyk8nz0jL1DWG"
+
+        // Do any additional setup after loading the view.
+    }
+
+    public override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("mi segue identifier \(segue.identifier)")
+        
+        if (segue.identifier == "WebSegue") {
+            let view2 = segue.destination as! MarketplaceViewController
+            //_ = sender as! [Int: Any?]
+            print("el apikey que voy a enviar \(apikeyText.text)")
+            print("el sessionId que voy a enviar \(sessionIdText.text)")
+            print("el locale que voy a enviar \(localeText.text)")
+            view2.apikey = apikeyText.text ?? ""
+            view2.sessionId = sessionIdText.text ?? ""
+            view2.locale = localeText.text ?? ""
+        }
+    }
+
+    @IBAction func goWebView(_ sender: Any) {
+        self.performSegue(withIdentifier: "WebSegue", sender: nil)
+    }
+}
+
