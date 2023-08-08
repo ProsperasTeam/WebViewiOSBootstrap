8d918dfbdfd63209787a45fec034a0e85df333a5
sayab perez <sayab@madhouse.com.mx>
2023-08-08 13:01:15 -0600

    arreglo camra
    arreglo camra

--- Marketplace Webview/ViewController.swift
+++ Marketplace Webview/ViewController.swift
@@ -23,9 +23,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         overrideUserInterfaceStyle = .light
         
-        apikeyText.text = "f0627e97-48fb-456b-ba05-c20ce8c56105"
+        apikeyText.text = "53788FFB-B0C2-48C7-929F-7130C2A52128"
+        //apikeyText.text = "f0627e97-48fb-456b-ba05-c20ce8c56105"
         localeText.text = "es-rMX"
-        sessionIdText.text = "d/fvIRnSFf2gQslGnCVm/pz7u8K5JTkEFY5w7KJ1rSawvF0tFj68wuL+Zuy2DrEkr8I/dqUj27iW0fg4RyMaU+jsqimIAsTVZnJjjYWpymAwyFRXa7rZyk8nz0jL1DWG"
+        //localeText.text = "es-rMX"
+        sessionIdText.text = "196743"
+        //sessionIdText.text = "d/fvIRnSFf2gQslGnCVm/pz7u8K5JTkEFY5w7KJ1rSawvF0tFj68wuL+Zuy2DrEkr8I/dqUj27iW0fg4RyMaU+jsqimIAsTVZnJjjYWpymAwyFRXa7rZyk8nz0jL1DWG"
 
         // Do any additional setup after loading the view.
     }
--- Marketplace Webview/ViewController2.swift
+++ Marketplace Webview/ViewController2.swift
@@ -20,6 +20,28 @@ class ViewController2: UIViewController {
     @IBOutlet weak var webView: WKWebView!
     
     
+    
+    override open func loadView() {
+        super.loadView()
+
+        let webConfiguration = WKWebViewConfiguration()
+        webConfiguration.ignoresViewportScaleLimits = true
+        webConfiguration.suppressesIncrementalRendering = true
+        webConfiguration.allowsInlineMediaPlayback = true
+        webConfiguration.allowsAirPlayForMediaPlayback = false
+        webConfiguration.allowsPictureInPictureMediaPlayback = true
+        webConfiguration.mediaTypesRequiringUserActionForPlayback = .all
+        webConfiguration.requiresUserActionForMediaPlayback = true
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.sizeToFit()
+        webView.backgroundColor = .black
+        webView.isOpaque = false
+        //self.webViewContainer.addSubview(webView)
+        view = webView
+
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         overrideUserInterfaceStyle = .light
@@ -57,12 +79,17 @@ class ViewController2: UIViewController {
     func abrirSDK(){
         print("mi info es apikey: \(apikey), locale: \(locale), sessionId: \(sessionId)")
         url = "https://creditos-web.prosperas.com?sessionid=\(sessionId)&apikey=\(apikey)&locale=\(locale)"
+        //url = "https://dev-prosperas.mdhs.mx/creditos_web/eazul/?fid=\(sessionId)&apikey=\(apikey)&locale=\(locale)"
         // Do any additional setup after loading the view.
+        
+    //https://dev-prosperas.mdhs.mx/creditos_web/eazul/?fid=196743&apikey=53788FFB-B0C2-48C7-929F-7130C2A52128&locale=es-rMX
         
         print("url recibida ::: \(url)")
         url = url.replacingOccurrences(of: " ", with: "")
         print("url ajustada ::: \(url)")
         webView.customUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Mobile/15E148 Safari/604.1"
+        
+        
         
         webView.load(NSURLRequest(url: NSURL(string: url)! as URL) as URLRequest)
         webView.allowsBackForwardNavigationGestures = true
